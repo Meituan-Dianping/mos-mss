4b8b6b4b502afd1fde566235539ecd4917e90515
xiaokunyu <xiaokunyu@meituan.com>
2019-10-18 10:51:45 +0800

    NODE_ENV 问题修复

--- src/config.js
+++ src/config.js
@@ -1,10 +1,9 @@
 'use strict';
-process.env.NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV : 'production';
 const conf = {
     production: 'mtmss.com'
 };
 
-const endpoint = conf[process.env.NODE_ENV];
+const endpoint = conf[process.env.NODE_ENV || 'production'];
 
 module.exports = {
     endpoint
